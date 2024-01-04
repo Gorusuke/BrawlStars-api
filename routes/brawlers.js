f5b30d40ca0531c63eeaf62f56beb8e04f9470ee
Jose Andres <Gorusuke123@gmail.com>
2024-01-04 13:05:19 -0500

    Add welcome message

--- routes/brawlers.js
+++ routes/brawlers.js
@@ -3,6 +3,9 @@ import { Brawlers } from '../controllers/brawlers.js'
 
 export const brawlesRouter = Router();
 
+brawlesRouter.get('/', (req, res) => {
+  res.send('<h1>Welcome to BS API</h1>')
+})
 brawlesRouter.get('/brawlers', Brawlers.getAllBrawlers)
 brawlesRouter.get('/brawlers/:id', Brawlers.getBrawler)
 brawlesRouter.get('/players/:id', Brawlers.getPlayer)
